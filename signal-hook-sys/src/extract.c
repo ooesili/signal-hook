0960461fb353b4fa0f7cc709a65b525694788117
Michal 'vorner' Vaner <vorner@vorner.cz>
2020-12-09 16:52:09 +0100

    fixup! Add the signal-hook-sys crate

--- signal-hook-sys/src/extract.c
+++ signal-hook-sys/src/extract.c
@@ -1,23 +1,51 @@
+#include <stdbool.h>
 #include <signal.h>
 #include <stdint.h>
 
-const uint8_t ORIGIN_UNKNOWN = 0;
-const uint8_t ORIGIN_PROCESS = 1;
-const uint8_t ORIGIN_KERNEL = 2;
+struct Const {
+    int native;
+    // The signal this applies to, or -1 if it applies to anything.
+    int signal;
+    uint8_t translated;
+};
 
-uint8_t sighook_signal_origin(const siginfo_t *info, pid_t *pid, uid_t *uid) {
-	switch (info->si_code) {
-		case SI_USER:
-		case SI_QUEUE:
-		case SI_MESGQ:
-			*pid = info->si_pid;
-			*uid = info->si_uid;
-			return ORIGIN_PROCESS;
+// Warning: must be in sync with the rust source code
+struct Const consts[] = {
 #ifdef SI_KERNEL
-		case SI_KERNEL:
-			return ORIGIN_KERNEL;
+    { SI_KERNEL, -1, 1 },
 #endif
-		default:
-			return ORIGIN_UNKNOWN;
-	}
+    { SI_USER, -1, 2 },
+#ifdef SI_TKILL
+    { SI_TKILL, -1, 3 },
+#endif
+    { SI_QUEUE, -1, 4 },
+    { SI_MESGQ, -1, 5 },
+    { CLD_EXITED, SIGCHLD, 6 },
+    { CLD_KILLED, SIGCHLD, 7 },
+    { CLD_DUMPED, SIGCHLD, 8 },
+    { CLD_TRAPPED, SIGCHLD, 9 },
+    { CLD_STOPPED, SIGCHLD, 10 },
+    { CLD_CONTINUED, SIGCHLD, 11 },
+};
+
+uint8_t sighook_signal_cause(const siginfo_t *info) {
+    const size_t const_len = sizeof consts / sizeof *consts;
+    size_t i;
+    for (i = 0; i < const_len; i ++) {
+        if (
+            consts[i].native == info->si_code &&
+            (consts[i].signal == -1 || consts[i].signal == info->si_signo)
+        ) {
+            return consts[i].translated;
+        }
+    }
+    return 0; // The "Unknown" variant
+}
+
+pid_t sighook_signal_pid(const siginfo_t *info) {
+    return info->si_pid;
+}
+
+uid_t sighook_signal_uid(const siginfo_t *info) {
+    return info->si_uid;
 }
